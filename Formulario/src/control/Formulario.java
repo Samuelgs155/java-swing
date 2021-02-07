16d7b0e31851ee659965172eca9f1dfa00b74fb4
Samuelgs155 <samuelgs15@hotmail.com>
2021-02-07 10:12:49 +0100

    Add files via upload

---
+++ Formulario/src/control/Formulario.java
@@ -0,0 +1,52 @@
+package control;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Formulario extends JFrame {
+
+	private JPanel contentPane;
+	private JLabel label1;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Formulario frame = new Formulario();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Formulario() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+        
+		label1=new JLabel("Hola Mundo.");
+        label1.setBounds(10,20,200,30);
+        contentPane.add(label1);
+	}
+	
+	
+
+}
